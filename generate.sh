25a26af7d418f2b532a11188a8ba41b36711aa4e
Di Zhang <zhangdi_me@163.com>
2017-05-14 00:08:12 +0800

    Update generate.sh

--- generate.sh
+++ generate.sh
@@ -4,15 +4,6 @@ BASE_DIR="$( cd "$( dirname "$0" )" && pwd )"
 YII2_DIR='/opt/code/github.com/yiisoft/yii2/'
 TARGET_DIR="${BASE_DIR}/apps/yiiguide"
 
-php () {
-    tty=
-    tty -s && tty=--tty
-    docker run \
-        $tty \
-        --rm \
-        php:7.0 "$@"
-}
-
 cd ${YII2_DIR} && \
 git pull && \
-php ${BASE_DIR}/build guide ${YII2_DIR} ${TARGET_DIR} --interactive=1 --overwrite=1
+docker run -it --rm php:7.0 php ${BASE_DIR}/build guide ${YII2_DIR} ${TARGET_DIR} --interactive=1 --overwrite=1
